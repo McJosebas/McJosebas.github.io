00e26e48348d0491699ebe32e9bedbd8b72d8d0b
McJosebas <mcjosebas@gmail.com>
2026-03-14 14:13:17 +0100

    cambio url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mcjosebas.github.io',
+    site: 'http://danielpr.com/',
     vite: {
         plugins: [tailwindcss()]
     },
